b4722e07628c49efdec2c043aedefed784128942
Andres Araya <araya.andres@gmail.com>
2015-05-03 15:38:03 -0600

    Formatting

--- Twelve/Twelve.cpp
+++ Twelve/Twelve.cpp
@@ -1,9 +1,9 @@
 #include "Twelve.h"
 
 Twelve::Twelve()
-: Gosu::Window(Game::ROWS * Square::SIDE + 2 * Game::BORDER, Game::COLS * Square::SIDE + 2 * Game::BORDER, false)
-, _font(this->graphics(), L"System", 72)
-, _game(this->graphics(), _font)
+    : Gosu::Window(Game::ROWS * Square::SIDE + 2 * Game::BORDER, Game::COLS * Square::SIDE + 2 * Game::BORDER, false)
+    , _font(this->graphics(), L"System", 72)
+    , _game(this->graphics(), _font)
 {
     setCaption(L"Twelve");
 }
@@ -29,7 +29,7 @@ void Twelve::buttonDown(Gosu::Button button)
 void Twelve::buttonUp(Gosu::Button button)
 {
     switch (button.id()) {
-            case Gosu::msLeft:
+        case Gosu::msLeft:
             _game.handle_mouse_up(input().mouseX(), input().mouseY());
             break;
             case Gosu::kbR:
